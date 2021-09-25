9ed5947ff4b2f0c207c04a70aade92485aa1655c
JvSchulz <vitorsm66@hotmail.com>
2021-09-25 01:39:46 -0300

    Implementação do Product

---
+++ src/main/java/br/com/jvschulz/gamestore/model/Product.java
@@ -0,0 +1,83 @@
+package br.com.jvschulz.gamestore.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+	
+	@Column(nullable = false)
+	public String name;
+	
+	public BigDecimal price;
+	public short score;
+	public String image;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public short getScore() {
+		return score;
+	}
+
+	public void setScore(short score) {
+		this.score = score;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return id == other.id;
+	}
+
+}
