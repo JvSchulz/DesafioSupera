e3ef16a6fec54feab9040181ad6a4a6c0e36f547
JvSchulz <vitorsm66@hotmail.com>
2021-09-25 19:16:14 -0300

    Implementado metodos de SortBy: Name, Price, Score

--- src/main/java/br/com/jvschulz/gamestore/controller/ProductController.java
+++ src/main/java/br/com/jvschulz/gamestore/controller/ProductController.java
@@ -3,7 +3,10 @@ package br.com.jvschulz.gamestore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +16,32 @@ import br.com.jvschulz.gamestore.repository.ProductRepository;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
 	@GetMapping
-	private List<Product> showProducts(){
+	private List<Product> showProducts() {
 		return productRepository.findAll();
+	}
+
+	@GetMapping(path = "/sortByName")
+	private List<Product> sortProductByName() {
+		return productRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+	}
+
+	@GetMapping(path = "/sortByPrice")
+	private List<Product> sortProductByPrice() {
+		return productRepository.findAll(Sort.by(Sort.Direction.ASC, "price"));
+	}
+
+	@GetMapping(path = "/sortByScore")
+	private List<Product> sortProductByScore() {
+		return productRepository.findAll(Sort.by(Sort.Direction.ASC, "score"));
+	}
+
+	@PostMapping(path = "/add")
+	public Product addProduct(@RequestBody Product product) {
+		return productRepository.save(product);
 	}
 }
