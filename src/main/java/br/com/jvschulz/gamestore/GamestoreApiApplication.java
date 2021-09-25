aaba8b4cdc7e0ee1ecbf4cd564559ff6748d915e
JvSchulz <vitorsm66@hotmail.com>
2021-09-25 00:57:57 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/jvschulz/gamestore/GamestoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jvschulz.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestoreApiApplication.class, args);
+	}
+
+}
