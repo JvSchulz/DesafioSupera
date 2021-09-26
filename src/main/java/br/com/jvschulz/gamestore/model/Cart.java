19eb6dbed88c8c07d0e55183b783fe7e86ec694c
JvSchulz <vitorsm66@hotmail.com>
2021-09-26 20:42:15 -0300

    Implementação do Cart

---
+++ src/main/java/br/com/jvschulz/gamestore/model/Cart.java
@@ -0,0 +1,84 @@
+package br.com.jvschulz.gamestore.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cart {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private BigDecimal subTotal;
+	private BigDecimal shippingFee;
+	private BigDecimal totalValue;
+	
+	@Column(nullable = false)
+	private List<ItemOrder> orderList;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getSubTotal() {
+		return subTotal;
+	}
+
+	public void setSubTotal(BigDecimal subTotal) {
+		this.subTotal = subTotal;
+	}
+
+	public BigDecimal getShippingFee() {
+		return shippingFee;
+	}
+
+	public void setShippingFee(BigDecimal shippingFee) {
+		this.shippingFee = shippingFee;
+	}
+
+	public BigDecimal getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(BigDecimal totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public List<ItemOrder> getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(List<ItemOrder> orderList) {
+		this.orderList = orderList;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cart other = (Cart) obj;
+		return id == other.id;
+	}
+	
+	
+}
