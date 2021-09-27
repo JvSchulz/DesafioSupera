436eece8728bec34900eebaf157bd456aea46b3e
JvSchulz <vitorsm66@hotmail.com>
2021-09-27 15:31:58 -0300

    Ajustes finais do projeto

--- src/main/java/br/com/jvschulz/gamestore/controller/CartController.java
+++ src/main/java/br/com/jvschulz/gamestore/controller/CartController.java
@@ -1,6 +1,8 @@
 package br.com.jvschulz.gamestore.controller;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +22,19 @@ public class CartController {
 	@Autowired
 	private ItemOrderRepository itemOrderRespository;
 
+	@GetMapping(path = "/add")
+	private void addCart() {
+		Cart cart = new Cart();
+		cartRepository.saveAndFlush(cart);
+	}
+	
 	@GetMapping
 	private Cart showCart() {
-		Cart cart = new Cart();
-		cart.setOrderList(itemOrderRespository.findAll());
-		cart.shipping(cart.getOrderList());
-		cart.subTotal(cart.getOrderList());
-		cart.totalValue();
-		return cartRepository.saveAndFlush(cart);
+		Optional<Cart> cart = cartRepository.findById(1L);
+		cart.get().setOrderList(itemOrderRespository.findAll());
+		cart.get().shipping(cart.get().getOrderList());
+		cart.get().subTotal(cart.get().getOrderList());
+		cart.get().totalValue();
+		return cartRepository.saveAndFlush(cart.get());
 	}
 }
